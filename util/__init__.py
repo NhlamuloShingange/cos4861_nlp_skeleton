6bc3f524c1247f00b9da004d9538632776a9313a
Nhlamulo <shingangenhlamulo@gmail.com>
2020-05-19 23:47:16 +0200

    Update __init__.py
    Question 6 - DistanceCalculator

--- util/__init__.py
+++ util/__init__.py
@@ -44,8 +44,38 @@ class DistanceCalculator:
     def distance(self, source, target):
         """
         Calculates the distance between two strings.
+        :param t:
+        :param s:
         :param source: The source string
         :param target: The target string
         :return: The scalar distance between the source and target.
         """
+        s = len(source)
+        t = len(target)
+        table = [[0 for j in range(s + 1)] for i in range(t + 1)]
+
+        for i in range(t + 1):
+            table[i][0] = i
+
+        for j in range(s + 1):
+            table[0][j] = j
+
+        for i in range(1, t + 1):
+            for j in range(1, s + 1):
+                if source[j-self._deletion_cost] == target[i-self._deletion_cost]:
+                    table[i][j] = table[i-self._deletion_cost][j-self._deletion_cost]
+                else:
+                    table[i][j] = min([table[i-self._insert_cost][j],
+                                       table[i][j-self._deletion_cost],
+                                       table[i-self._subst_cost][j-self._subst_cost]]) + 1
+
+        return table[len(target)][len(source)]
+
         raise NotImplementedError('Distance calculation not implemented yet')
+
+
+test = DistanceCalculator()
+s = input("Enter first word : ")
+t = input("Enter second word : ")
+result = test.distance(s, t)
+print(result)
